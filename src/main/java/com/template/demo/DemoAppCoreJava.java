3eeb240d27c0636d544262ac3904e85f3ad634a3
Prashant Patel <ppatel@calibo.com>
2024-06-21 18:33:52 +0530

    Upgrade to tomcat 10.1.5

--- src/main/java/com/template/demo/DemoAppCoreJava.java
+++ src/main/java/com/template/demo/DemoAppCoreJava.java
@@ -1,13 +1,13 @@
 package com.template.demo;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
