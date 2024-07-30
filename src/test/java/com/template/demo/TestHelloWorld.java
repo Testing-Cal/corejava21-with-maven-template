872765434400768f772a565afca7aca91f15e39e
s.gadekar <sgadekar@calibo.com>
2024-07-30 10:09:34 +0530

    added maven wrapper and Junit 5  version. Jenkins file changes added

--- src/test/java/com/template/demo/TestHelloWorld.java
+++ src/test/java/com/template/demo/TestHelloWorld.java
@@ -1,8 +1,8 @@
 package com.template.demo;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 /*Test*/
 public class TestHelloWorld {
